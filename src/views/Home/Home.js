c91a855034bdce57158a8f3fac32400ede071026
Lea <lea.syhanath@uavia.eu>
2019-01-11 17:21:28 +0100

    content almost done

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,32 +3,24 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Logo from '../../styles/img/logo.png';
 import shapeHome from '../../styles/img/morceau-home.png';
-import shape1 from '../../styles/img/morceau-1.png';
-import shape2 from '../../styles/img/morceau-2.png';
-import shape3 from '../../styles/img/morceau-3.png';
-import shape4 from '../../styles/img/morceau-4.png';
-import shape5 from '../../styles/img/morceau-5.png';
-import shape6 from '../../styles/img/morceau-6.png';
-import shape7 from '../../styles/img/morceau-7.png';
-import shape8 from '../../styles/img/morceau-8.png';
 
 import './Home.css';
 
-const Home = ({ categories }) => (
+const Home = ({ categories, images }) => (
   <section>
     <div className="content">
+
       <div className="homepage">
       <div className="logo-content">
       <img className="logo" src={Logo} alt="LAME"/>
       </div>
     <div className="figure">
-      <span className="home-text">PRESS & HOLD</span>
+      <span className="home-text">CLICK HERE</span>
       <img className="shape-home" src={shapeHome} alt="HOME"/>
     </div>
       </div>
 
-    
-    {categories.length > 0 && (
+    { categories.length && images.length > 0 && (
         <section>
         <h1 className="choose-title">CHOOSE YOUR CATEGORY</h1>
             <ul className="categories-list">
@@ -37,7 +29,9 @@ const Home = ({ categories }) => (
                     <Link to={`/categories/${category.id}`} key={category.id} >
                   <span className="category-title"> {category.title}</span>
                     </Link>
-                <img className="shape-category" src={shape1} alt="" />
+
+                <img className="shape-category" src={images[1]} alt="img" />
+
                 </li>
             ))}
             </ul>
--- src/views/Home/HomeContainer.js
+++ src/views/Home/HomeContainer.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import shape1 from '../../styles/img/morceau-1.png';
+import shape2 from '../../styles/img/morceau-2.png';
+import shape3 from '../../styles/img/morceau-3.png';
+import shape4 from '../../styles/img/morceau-4.png';
+import shape5 from '../../styles/img/morceau-5.png';
+import shape6 from '../../styles/img/morceau-6.png';
+import shape7 from '../../styles/img/morceau-7.png';
+import shape8 from '../../styles/img/morceau-8.png';
 
 class HomeContainer extends Component {
   state = {
     categories: [],
+
+      images: [
+          shape1,
+          shape2,
+          shape3,
+          shape4,
+          shape5,
+          shape6,
+          shape7,
+          shape8,
+          shape1,
+          shape2
+      ],
+
+
   }
+
+
   componentDidMount() {
     fetch('http://jservice.io/api/categories?count=10').then(response => {
       response.json().then(categories => {
@@ -17,7 +42,7 @@ class HomeContainer extends Component {
   }
   render() {
     return (
-      <Home categories={this.state.categories} />
+      <Home categories={this.state.categories} images={this.state.images}/>
     );
   }
 }
