4d51b71a599cd7864e90132f0f9cc423f09dadb1
Manon Ragnotti <manon.ragnotti@hotmail.fr>
2019-01-13 21:34:12 +0100

    Add Game Over and Succes

--- src/views/Popin/GameOver.js
+++ src/views/Popin/GameOver.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router'
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Stockage  from '../../helpers/Stockage';
 
+import popin from '../../styles/popin.css';
+import index from '../../styles/index.css';
+
 const GameOver = ({life}) => (
-  <section>
+  <section className="gameover">
     <h1 className="logo">LAME</h1>
-    <section>
+    <section className="container">
       <span className="backText">TRY AGAIN</span>
       <span className="frontText">To win, you need to loose.</span>
-      <form onSubmit={e => Stockage.gameOver(e)} Redirect to="/">
-        <input type="submit" value="REPLAY"/>
-      </form>
+      <Link className='buttonReplay' to={'/'}>REPLAY</Link>
     </section>
   </section>
 );
--- src/views/Popin/Succes.js
+++ src/views/Popin/Succes.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Stockage  from '../../helpers/Stockage';
 
+import popin from '../../styles/popin.css';
+import index from '../../styles/index.css';
+
 const Succes = ({score}) => (
-  <section>
+  <section className="success">
     <h1 className="logo">LAME</h1>
-    <section>
+    <section className="container">
       <span className="backText">GOOD JOB</span>
       <span className="frontText">Obstinact is the key to success.</span>
+      <Link className='buttonReplay' to={'/'}>REPLAY</Link>
     </section>
   </section>
 );
