5689bd2fcc9df0b4e68157765d7232ec9e830f85
Manon Ragnotti <manon.ragnotti@hotmail.fr>
2019-01-13 21:36:19 +0100

    Update App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import styled from 'styled-components';
-import './App.css';
-
 import HomeContainer from '../../views/Home/HomeContainer';
 import CategoryContainer from '../../views/Category/CategoryContainer';
 import GameOver from '../../views/Popin/GameOver';
@@ -12,7 +9,7 @@ import Succes from '../../views/Popin/Succes';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Router>
           <Fragment>
             <Route exact path="/" component={HomeContainer} />
